5726317fae3bbb4ed7995f6a4fff5fdb031d375d
pvsile <pvsile@utu.fi>
2018-12-12 22:24:57 +0200

    add schema

--- models/person.js
+++ models/person.js
@@ -4,17 +4,21 @@ const url = 'mongodb://fullstack:A49NVuTjduafn2L@ds137650.mlab.com:37650/phonebo
 
 mongo.connect(url)
 
-const Person = mongo.model('Person',{
-    name: String,
-    number: String,
-})
+const personSchema = new mongoose.Schema(
+    {
+        name: String,
+        number: String
+    }
+)
 
-Person.statics.format = (person) => (
+personSchema.statics.format = (person) => (
     {
         name: person.name,
         number: person.number,
         id: person._id
     }
 )
+
+const Person = mongoose.model('Person', personSchema)
 
 module.ecports = Person
