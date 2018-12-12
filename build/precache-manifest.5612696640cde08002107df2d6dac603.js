822f9b674eaf68b209d187ab976daf29aa8468a8
pvsile <pvsile@utu.fi>
2018-12-12 13:47:45 +0200

    add front to heroku

---
+++ build/precache-manifest.5612696640cde08002107df2d6dac603.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "2016894c7b6a3ba6569a",
+    "url": "/static/css/main.9cf9346c.chunk.css"
+  },
+  {
+    "revision": "2016894c7b6a3ba6569a",
+    "url": "/static/js/main.2016894c.chunk.js"
+  },
+  {
+    "revision": "efb9f3153127ac881aa4",
+    "url": "/static/js/1.efb9f315.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "83dbb98ec62ff7657ac0ceee3ec140ce",
+    "url": "/index.html"
+  }
+];
